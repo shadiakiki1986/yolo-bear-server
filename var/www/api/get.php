6128ccf6561e450d70b7c0d0007cfe687586b9ce
Shadi Akiki <shadiakiki1986@gmail.com>
2015-02-28 13:07:58 +0200

    added no cache to php headers in api

--- var/www/api/get.php
+++ var/www/api/get.php
@@ -1,5 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 /*
  Returns a selected yolo-bear tournament
