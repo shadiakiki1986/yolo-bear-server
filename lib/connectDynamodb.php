2302c97f2232ac3bd02ba6e763f5ddb43217a6db
Shadi Akiki <shadiakiki1986@gmail.com>
2015-07-23 05:55:57 +0000

    added 'latest' to version of dynamodb

--- lib/connectDynamodb.php
+++ lib/connectDynamodb.php
@@ -6,6 +6,7 @@ use Aws\DynamoDb\DynamoDbClient;
 
 function connectDynamodb() {
 return 	DynamoDbClient::factory(array(
+    'version' => 'latest',
     'key' => AWS_KEY, # check config file
     'secret'  => AWS_SECRET,
     'region'  => AWS_REGION
