8eff7a0c58c37c04e2c0590b58d60fc77396db9a
Shadi Akiki <shadiakiki1986@gmail.com>
2015-07-23 09:06:56 +0000

    with aws-php-sdk 3.2, I updated my connectDynmodb.php file

--- lib/connectDynamodb.php
+++ lib/connectDynamodb.php
@@ -5,11 +5,16 @@ require_once dirname(__FILE__).'/../config.php';
 use Aws\DynamoDb\DynamoDbClient;
 
 function connectDynamodb() {
-return 	DynamoDbClient::factory(array(
+  $neededVars=array("AWS_KEY", "AWS_SECRET", "AWS_REGION");
+  foreach($neededVars as $nv) if(!defined($nv)) throw new Exception("Please define the $nv variable in the config file");
+
+  return DynamoDbClient::factory(array(
     'version' => 'latest',
-    'key' => AWS_KEY, # check config file
-    'secret'  => AWS_SECRET,
-    'region'  => AWS_REGION
-));
+    'region'  => AWS_REGION,
+    'credentials' => array(
+      'key' => AWS_KEY, # check config file
+      'secret'  => AWS_SECRET,
+    )
+  ));
 }
 
